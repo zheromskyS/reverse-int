5932aa01a31a14bf401671ed80f107f0609fa97a
Stanislau Zheromski <stanislawzheromsky@gmail.com>
2023-04-30 23:05:58 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    let stringFromNumber = String(number);
+    let arrayFromString = [];
+    let newStringFromArray = 0;
+    let reversedNumber = 0;
+
+    for (let element of stringFromNumber) {
+        arrayFromString.push(element);
+    }
+
+    arrayFromString.reverse();
+    newStringFromArray = arrayFromString.join('');
+
+    if (number < 0) {
+        reversedNumber = Number(newStringFromArray.slice(0, newStringFromArray.length - 1));
+    } else {
+        reversedNumber = Number(newStringFromArray);
+    }
+
+    return reversedNumber;
 }
